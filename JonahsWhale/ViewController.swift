46e7d387213311335bf75b4244c216ee35c98bd8
michael febrianto <michaelfebrianto@gmail.com>
2016-08-21 20:22:00 +1000

    first commit

---
+++ JonahsWhale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JonahsWhale
+//
+//  Created by michael febrianto on 21/08/2016.
+//  Copyright © 2016 mfebrianto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
